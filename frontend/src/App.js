dd9bae955a71fa939cdf54f20b20927c940b86b4
Jonathan14853 <nathanwalumbe@gmail.com>
2021-03-07 23:45:47 +0300

    E-shop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
               <i className="fa fa-bars"></i>
             </button>
             <Link className="brand" to="/">
-              amazona
+              E-SHOP
             </Link>
           </div>
           <div>
